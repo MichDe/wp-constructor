d95a1e395ace91339f62146dc72cbdea50890c0c
AntonShevchuk <AntonShevchuk@545853b8-13c7-11de-a6cc-771c510851eb>
2009-06-12 16:45:52 +0000

    slideshow in progress

--- footer.php
+++ footer.php
@@ -4,30 +4,8 @@
  * @subpackage Constructor
  */
 ?>
-	<div id="footer">
-	
-		<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer') ) : ?>
-	    	
-        	<div>
-        	    <h3><?php _e('Categories', 'constructor')?></h3>
-        		<ul>                       
-        			<?php wp_list_categories('title_li='); ?>
-        		</ul>
-    		</div>
-			<div>
-	    	    <h3><?php _e('Archives', 'constructor') ?></h3>
-    			<ul>
-    			    <?php wp_get_archives('type=monthly&limit=12'); ?>
-    			</ul>
-        	</div>     
-        	<div>
-        	    <h3><?php _e('Blogroll', 'constructor')?></h3>
-        	    <ul>
-                    <?php wp_list_bookmarks('title_li=&categorize=0'); ?>
-                </ul>
-        	</div>
-		<?php endif; ?>
+	<div id="footer">	
+		<?php if ( function_exists('dynamic_sidebar')) { dynamic_sidebar('footer'); } ?>
     	<p class="clear copy">
         	<?php get_constructor_footer(); ?>
     	</p>
