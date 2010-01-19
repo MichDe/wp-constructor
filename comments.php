cd6470d82c2c2137e269e9c32eac7ada2c25dd04
AntonShevchuk <AntonShevchuk@545853b8-13c7-11de-a6cc-771c510851eb>
2010-01-19 09:40:41 +0000

    hot fix in comments layout

--- comments.php
+++ comments.php
@@ -18,7 +18,7 @@
 <!-- You can start editing here. -->
 
 <?php if ( have_comments() ) : ?>
-	<h3 id="comments"><?php comments_number(__('No Responses', 'constructor'), __('One Response', 'constructor'), __('% Responses', 'constructor'));?> <?php printf(__('to &#8220;%s&#8221;', 'constructor'), the_title('', '', false)); ?></h3>
+	<h3><?php comments_number(__('No Responses', 'constructor'), __('One Response', 'constructor'), __('% Responses', 'constructor'));?> <?php printf(__('to &#8220;%s&#8221;', 'constructor'), the_title('', '', false)); ?></h3>
 
 	<ol class="commentlist">
 	<?php wp_list_comments('avatar_size='.get_constructor_avatar_size());?>
@@ -29,15 +29,12 @@
 		<div class="alignright"><?php next_comments_link() ?></div>
 	</div>
 
- <?php else : // this is displayed if there are no comments so far ?>
-
+<?php else : // this is displayed if there are no comments so far ?>
 	<?php if ('open' == $post->comment_status) : ?>
-		<!-- If comments are open, but there are no comments. -->
-
-	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
-		<p class="nocomments"><?php _e('Comments are closed.', 'constructor'); ?></p>
-
+	<!-- If comments are open, but there are no comments. -->
+	<?php else : // comments are closed ?>
+	<!-- If comments are closed. -->
+	<p class="nocomments"><?php _e('Comments are closed.', 'constructor'); ?></p>
 	<?php endif; ?>
 <?php endif; ?>
 
@@ -47,44 +44,33 @@
 <div id="respond">
 
 <h3><?php comment_form_title( __('Leave a Reply', 'constructor'), __('Leave a Reply for %s' , 'constructor') ); ?></h3>
-
 <div id="cancel-comment-reply"> 
 	<small><?php cancel_comment_reply_link() ?></small>
 </div> 
 
 <?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-<p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'constructor'), get_option('siteurl') . '/wp-login.php?redirect_to=' . urlencode(get_permalink())); ?></p>
+    <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'constructor'), get_option('siteurl') . '/wp-login.php?redirect_to=' . urlencode(get_permalink())); ?></p>
 <?php else : ?>
 
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-<?php comment_id_fields(); ?> 
-<?php if ( $user_ID ) : ?>
-
-<p><?php printf(__('Logged in as <a href="%1$s">%2$s</a>.', 'constructor'), get_option('siteurl') . '/wp-admin/profile.php', $user_identity); ?> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account', 'constructor'); ?>"><?php _e('Log out &raquo;', 'constructor'); ?></a></p>
-
-<?php else : ?>
-
-<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="author"><?php _e('Name', 'constructor'); ?> <?php if ($req) _e("(required)", "constructor"); ?></label></p>
-
-<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="email"><?php _e('Mail (will not be published)', 'constructor'); ?> <?php if ($req) _e("(required)", "constructor"); ?></label></p>
-
-<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-<label for="url"><?php _e('Website', 'constructor'); ?></label></p>
-
-<?php endif; ?>
-
-<!--<p><small><?php printf(__('<strong>XHTML:</strong> You can use these tags: <code>%s</code>', 'constructor'), allowed_tags()); ?></small></p>-->
-
-<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
-
-<p class="submit"><input name="submit" type="submit" id="submit" tabindex="5" class="medium awesome" value="<?php _e('Submit Comment', 'constructor'); ?>" /></p>
-<?php do_action('comment_form', $post->ID); ?>
-
-</form>
-</div>
+    <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+    <?php comment_id_fields(); ?> 
+    <?php if ( $user_ID ) : ?>    
+        <p><?php printf(__('Logged in as <a href="%1$s">%2$s</a>.', 'constructor'), get_option('siteurl') . '/wp-admin/profile.php', $user_identity); ?> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account', 'constructor'); ?>"><?php _e('Log out &raquo;', 'constructor'); ?></a></p>    
+    <?php else : ?>
+        <p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+        <label for="author"><?php _e('Name', 'constructor'); ?> <?php if ($req) _e("(required)", "constructor"); ?></label></p>        
+        <p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+        <label for="email"><?php _e('Mail (will not be published)', 'constructor'); ?> <?php if ($req) _e("(required)", "constructor"); ?></label></p>        
+        <p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />        
+        <label for="url"><?php _e('Website', 'constructor'); ?></label></p>
+    
+    <?php endif; ?>    
+    <!--<p><small><?php printf(__('<strong>XHTML:</strong> You can use these tags: <code>%s</code>', 'constructor'), allowed_tags()); ?></small></p>-->    
+    <p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>    
+    <p class="submit"><input name="submit" type="submit" id="submit" tabindex="5" class="medium awesome" value="<?php _e('Submit Comment', 'constructor'); ?>" /></p>
+    <?php do_action('comment_form', $post->ID); ?>    
+    </form>
 
 <?php endif; // If registration required and not logged in ?>
-
+</div><!--id="respond"-->    
 <?php endif; // if you delete this the sky will fall on your head ?>
