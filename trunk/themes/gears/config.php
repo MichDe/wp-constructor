876d7f59d9f02b2d7ce75a595b2481e6814541b6
AntonShevchuk <AntonShevchuk@545853b8-13c7-11de-a6cc-771c510851eb>
2011-04-08 09:40:03 +0000

    Added Gears theme

---
+++ trunk/themes/gears/config.php
@@ -0,0 +1,240 @@
+<?php 
+/* Save on 2011-04-08 09:18 */ 
+return array (
+  'sidebar' => 'none',
+  'layout' => 
+  array (
+    'header' => '286',
+    'width' => '580',
+    'sidebar' => '120',
+    'extra' => '120',
+    'fluid' => 
+    array (
+      'flag' => false,
+      'width' => 80,
+      'min-width' => 960,
+      'max-width' => 1280,
+    ),
+    'home' => 'default',
+    'index' => 'default',
+    'page' => 'page',
+    'single' => 'single',
+    'archive' => 'default',
+    'date' => 'default',
+    'category' => 'default',
+    'tag' => 'default',
+    'search' => 'default',
+  ),
+  'title' => 
+  array (
+    'pos' => 'center bottom',
+    'hidden' => false,
+  ),
+  'content' => 
+  array (
+    'date' => true,
+    'links' => 
+    array (
+      'author' => false,
+      'category' => true,
+      'tags' => false,
+      'comments' => true,
+    ),
+    'widget' => 
+    array (
+      'flag' => false,
+      'after' => 1,
+    ),
+    'social' => 
+    array (
+      'twitter' => '1',
+      'facebook' => '1',
+      'delicious' => '1',
+      'reddit' => '1',
+      'vkontakte' => '0',
+      'digg' => '0',
+      'mixx' => '0',
+      'stumbleupon' => '0',
+      'google' => '0',
+      'memori' => '0',
+    ),
+  ),
+  'comments' => 
+  array (
+    'avatar' => 
+    array (
+      'size' => '64',
+      'pos' => 'right',
+    ),
+  ),
+  'footer' => 
+  array (
+    'text' => '',
+  ),
+  'fonts' => 
+  array (
+    'title' => 
+    array (
+      'family' => '"Coming Soon"',
+      'size' => '64',
+      'weight' => '800',
+      'color' => '#453201',
+      'transform' => 'capitalize',
+    ),
+    'description' => 
+    array (
+      'family' => '"Coming Soon"',
+      'size' => '14',
+      'weight' => '700',
+      'color' => '#571c01',
+      'transform' => 'capitalize',
+    ),
+    'header' => 
+    array (
+      'family' => '"Coming Soon"',
+    ),
+    'content' => 
+    array (
+      'family' => 'Schoolbell',
+    ),
+  ),
+  'menu' => 
+  array (
+    'pos' => 'left top',
+    'width' => false,
+    'flag' => false,
+    'home' => false,
+    'rss' => false,
+    'search' => false,
+    'pages' => 
+    array (
+      'depth' => 1,
+      'exclude' => '',
+    ),
+    'categories' => 
+    array (
+      'depth' => 1,
+      'exclude' => '',
+      'group' => 1,
+      'title' => '',
+    ),
+  ),
+  'slideshow' => 
+  array (
+    'flag' => false,
+    'layout' => 'in',
+    'onpage' => false,
+    'onsingle' => false,
+    'onarchive' => false,
+    'showposts' => 10,
+    'id' => 0,
+    'height' => 200,
+    'advanced' => 
+    array (
+      'play' => false,
+      'effect' => 'slide',
+      'effectTime' => 300,
+      'timeout' => 3000,
+      'thumb' => false,
+    ),
+  ),
+  'design' => 
+  array (
+    'box' => 
+    array (
+      'flag' => false,
+      'radius' => 6,
+    ),
+    'shadow' => 
+    array (
+      'flag' => false,
+      'x' => 0,
+      'y' => 0,
+      'blur' => 3,
+    ),
+  ),
+  'images' => 
+  array (
+    'body' => 
+    array (
+      'src' => 'header.jpg',
+      'pos' => 'center top',
+      'repeat' => 'no-repeat',
+      'fixed' => false,
+    ),
+    'wrap' => 
+    array (
+      'src' => '',
+      'pos' => 'center top',
+      'repeat' => 'no-repeat',
+      'fixed' => false,
+    ),
+    'header' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapper' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'sidebar' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'extrabar' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'footer' => 
+    array (
+      'src' => 'footer.jpg',
+      'pos' => 'center top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapheader' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapcontent' => 
+    array (
+      'src' => 'wrapper.jpg',
+      'pos' => 'center top',
+      'repeat' => 'repeat-y',
+    ),
+    'wrapfooter' => 
+    array (
+      'src' => 'wrapperfooter.jpg',
+      'pos' => 'center top',
+      'repeat' => 'repeat-x',
+    ),
+  ),
+  'opacity' => 'none',
+  'color' => 
+  array (
+    'bg' => '#ffe5bd',
+    'bg2' => '#fffee5',
+    'opacity' => '#000',
+    'title' => '#000000',
+    'title2' => '#404040',
+    'text' => '#452707',
+    'text2' => '#331900',
+    'border' => '#555',
+    'border2' => '#999',
+    'form' => '#fff1cc',
+    'header1' => '#823400',
+    'header2' => '#a34700',
+    'header3' => '#c76400',
+  ),
+  'theme-reload' => '0',
+)
+ ?>
